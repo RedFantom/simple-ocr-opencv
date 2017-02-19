f0187897cde491dd28eeec2ece2264209de20f4c
RedFantom <redfantom@outlook.com>
2017-02-19 15:54:47 +0100

    Added Improver for #4 and testing for class

--- testing.py
+++ testing.py
@@ -5,22 +5,25 @@
 import unittest
 from files import ImageFile
 from grounding import TextGrounder
-from segmentation import ContourSegmenter
+from segmentation import ContourSegmenter, RawContourSegmenter
 from feature_extraction import SimpleFeatureExtractor
 from classification import KNNClassifier
 from ocr import OCR, reconstruct_chars
+import improver
+from PIL import Image
+import os
 
 
 class Testing(unittest.TestCase):
     def test_grounding_digits(self):
         grounder = TextGrounder()
-        self.assertTrue(grounder)
+        self.assertIsInstance(grounder, TextGrounder)
         digits = ImageFile('digits1')
-        self.assertTrue(digits)
+        self.assertIsInstance(digits, ImageFile)
         segmenter = ContourSegmenter()
-        self.assertTrue(segmenter)
+        self.assertIsInstance(segmenter, ContourSegmenter)
         segments = segmenter.process(digits.image)
-        # self.assertTrue(segments)
+        self.assertEqual(len(segments), 125)
         characters = ['9', '8', '2', '1', '4', '8', '0', '8', '6', '5', '1', '3', '2', '8',
                       '2', '3', '0', '6', '6', '4', '7', '0', '9', '3', '8', '4', '4', '6',
                       '0', '9', '5', '5', '0', '5', '8', '2', '2', '3', '1', '7', '2', '5',
@@ -52,21 +55,34 @@ class Testing(unittest.TestCase):
 
     def test_ocr_digits(self):
         segmenter = ContourSegmenter()
-        self.assertTrue(segmenter)
         extractor = SimpleFeatureExtractor()
-        self.assertTrue(extractor)
         classifier = KNNClassifier()
-        self.assertTrue(classifier)
         ocr = OCR(segmenter, extractor, classifier)
-        self.assertTrue(ocr)
         ocr.train(ImageFile('digits1'))
         digits = ImageFile('digits2')
-        self.assertTrue(digits)
         classes, segments = ocr.ocr(digits, show_steps=False)
         self.assertEqual(reconstruct_chars(classes), "31415926535897932384626433832795028841971693993751058209749445923"
                                                      "07816406286208998628034825342117067982148086513282306647093844609"
                                                      "55058223172535940812848111745028410270193852110555964462294895493"
                                                      "038196442881097566593344612847")
+
+    def test_pillow_imagefile_conversion(self):
+        imagefile = ImageFile('digits1')
+        self.assertIsInstance(imagefile, ImageFile)
+        pillow = improver.imagefile_to_pillow(imagefile)
+        self.assertIsInstance(pillow, Image.Image)
+
+    def test_improver_class_segmentation(self):
+        for name in [name for name in os.listdir(os.getcwd() + "/data") if name.startswith("timer")]:
+            digits = ImageFile(name)
+            impr = improver.ImageFileImprover(digits)
+            impr.crop((0, 20, 70, 40))
+            impr.enhance(color=0.0, brightness=1.0, contrast=1.0, sharpness=1.0, invert=True)
+            digits_impr = impr.imagefile
+            segmenter = RawContourSegmenter(blur_x=5, blur_y=5)
+            segments = segmenter.process(digits_impr.image)
+            self.assertTrue(len(segments) >= 4)
+
 
 unittest.main()
 
