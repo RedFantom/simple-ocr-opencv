11881c56b7ecff523823f66f8dfe03a5d838093a
RedFantom <redfantom@outlook.com>
2017-02-20 13:47:16 +0100

    Added TerminalGrounder and tests for #7

--- testing.py
+++ testing.py
@@ -3,8 +3,9 @@
 # All additions are under the copyright of their respective authors
 # For license see LICENSE
 import unittest
+import mock
 from files import ImageFile
-from grounding import TextGrounder
+from grounding import TextGrounder, TerminalGrounder
 from segmentation import ContourSegmenter, RawContourSegmenter
 from feature_extraction import SimpleFeatureExtractor
 from classification import KNNClassifier
@@ -82,6 +83,30 @@ class Testing(unittest.TestCase):
             segmenter = RawContourSegmenter(blur_x=5, blur_y=5)
             segments = segmenter.process(digits_impr.image)
             self.assertTrue(len(segments) >= 4)
+
+    def test_terminal_grounder(self):
+        terminal = TerminalGrounder()
+        segmenter = ContourSegmenter()
+        image = ImageFile('digits1')
+        segments = segmenter.process(image.image)
+        with mock.patch('__builtin__.raw_input', mock_input):
+            terminal.ground(image, segments)
+
+current_char = 0
+characters = ['9', '8', '2', '1', '4', '8', '0', '8', '6', '5', '1', '3', '2', '8',
+              '2', '3', '0', '6', '6', '4', '7', '0', '9', '3', '8', '4', '4', '6',
+              '0', '9', '5', '5', '0', '5', '8', '2', '2', '3', '1', '7', '2', '5',
+              '3', '5', '9', '4', '0', '8', '1', '2', '8', '4', '8', '1', '1', '1',
+              '7', '4', '5', '0', '2', '8', '4', '1', '0', '2', '7', '0', '1', '9',
+              '3', '8', '5', '2', '1', '1', '0', '5', '5', '5', '9', '6', '4', '4',
+              '6', '2', '2', '9', '4', '8', '9', '5', '4', '9', '3', '0', '3', '8',
+              '1', '9', '6', '4', '4', '2', '8', '8', '1', '0', '9', '7', '5', '6',
+              '6', '5', '9', '3', '3', '4', '4', '6', '1', '2', '8', '4', '7']
+mock_input_gen = (char for char in characters)
+
+
+def mock_input(prompt):
+    return next(mock_input_gen)
 
 
 unittest.main()
