3b3ee2ea5b0faf00e770f5eaeedb1c2ef051a148
RedFantom <redfantom@outlook.com>
2017-02-28 20:15:35 +0100

    Added UserGrounder test

--- testing.py
+++ testing.py
@@ -14,7 +14,7 @@ import opencv_utils
 import improver
 from PIL import Image
 import os
-from StringIO import StringIO
+import cv2
 
 
 class TestGrounders(unittest.TestCase):
@@ -93,7 +93,17 @@ class TestGrounders(unittest.TestCase):
         segmenter = ContourSegmenter()
         image = ImageFile('digits1')
         segments = segmenter.process(image.image)
+        characters = ['9', '8', '2', '1', '4', '8', '0', '8', '6', '5', '1', '3', '2', '8',
+                      '2', '3', '0', '6', '6', '4', '7', '0', '9', '3', '8', '4', '4', '6',
+                      '0', '9', '5', '5', '0', '5', '8', '2', '2', '3', '1', '7', '2', '5',
+                      '3', '5', '9', '4', '0', '8', '1', '2', '8', '4', '8', '1', '1', '1',
+                      '7', '4', '5', '0', '2', '8', '4', '1', '0', '2', '7', '0', '1', '9',
+                      '3', '8', '5', '2', '1', '1', '0', '5', '5', '5', '9', '6', '4', '4',
+                      '6', '2', '2', '9', '4', '8', '9', '5', '4', '9', '3', '0', '3', '8',
+                      '1', '9', '6', '4', '4', '2', '8', '8', '1', '0', '9', '7', '5', '6',
+                      '6', '5', '9', '3', '3', '4', '4', '6', '1', '2', '8', '4', '7']
         mock_input_gen = (char for char in characters)
+
         def mock_input(prompt):
             return next(mock_input_gen)
         with mock.patch('__builtin__.raw_input', mock_input):
@@ -116,6 +126,34 @@ class TestGrounders(unittest.TestCase):
         segments = segmenter.process(image.image)
         with mock.patch('__builtin__.raw_input', return_value="exit"):
             self.assertRaises(AssertionError, lambda: terminal.ground(image, segments))
+
+    def test_user_grounder(self):
+        characters = [57, 56, 50, 49, 52, 56, 48, 56, 54, 53, 49, 51, 50, 56, 50, 51, 48, 54, 54, 52, 55, 48, 57, 51,
+                      56, 52, 52, 54, 48, 57, 53, 53, 48, 53, 56, 50, 50, 51, 49, 55, 50, 53, 51, 53, 57, 52, 48, 56,
+                      49, 50, 56, 52, 56, 49, 49, 49, 55, 52, 53, 48, 50, 56, 52, 49, 48, 50, 55, 48, 49, 57, 51, 56,
+                      53, 50, 49, 49, 48, 53, 53, 53, 57, 54, 52, 52, 54, 50, 50, 57, 52, 56, 57, 53, 52, 57, 51, 48,
+                      51, 56, 49, 57, 54, 52, 52, 50, 56, 56, 49, 48, 57, 55, 53, 54, 54, 53, 57, 51, 51, 52, 52, 54,
+                      49, 50, 56, 52, 55, 27]
+        mock_generator = (char for char in characters)
+
+        def mock_input(*args):
+            return next(mock_generator)
+        grounder = UserGrounder()
+        segmenter = ContourSegmenter()
+        image = ImageFile('digits1')
+        segments = segmenter.process(image.image)
+        with mock.patch('cv2.waitKey', mock_input):
+            grounder.ground(image, segments)
+        extractor = SimpleFeatureExtractor()
+        classifier = KNNClassifier()
+        ocr = OCR(segmenter, extractor, classifier)
+        ocr.train(ImageFile('digits1'))
+        digits = ImageFile('digits2')
+        classes, segments = ocr.ocr(digits, show_steps=False)
+        self.assertEqual(reconstruct_chars(classes), "31415926535897932384626433832795028841971693993751058209749445923"
+                                                     "07816406286208998628034825342117067982148086513282306647093844609"
+                                                     "55058223172535940812848111745028410270193852110555964462294895493"
+                                                     "038196442881097566593344612847")
 
 
 class TestOCR(unittest.TestCase):
