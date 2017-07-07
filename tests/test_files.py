fe6580de4a32b0b1f2204e08fec683527053c286
RedFantom <redfantom@outlook.com>
2017-07-07 11:43:59 +0200

    Add test to test opening custom files

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,5 +1,9 @@
 import unittest
 from simpleocr.files import ImageFile
+from simpleocr.files import DATA_DIRECTORY
+import os
+import tempfile
+from shutil import copyfile
 
 TEST_FILE = 'digits1'
 UNICODE_TEST_FILE = 'unicode1'
@@ -21,3 +25,11 @@ class TestImageFile(unittest.TestCase):
         self.assertEqual(imgf.is_grounded, True)
         imgf.remove_ground(remove_file=False)
         self.assertEqual(imgf.is_grounded, False)
+
+    def test_open_custom(self):
+        # First copy the file to a reachable folder
+        digits_path = os.path.join(DATA_DIRECTORY, "digits1.png")
+        destination = os.path.join(os.getcwd(), "digits_custom.png")
+        copyfile(digits_path, destination)
+        file = ImageFile("digits_custom")
+        self.assertIsInstance(file, ImageFile)
